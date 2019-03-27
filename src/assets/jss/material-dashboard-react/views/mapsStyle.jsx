b8524d4ee9a426b62b1a52318f1484d540b679cf
Jose Manuel Beauregard Mendez <jm.beauregard26@gmail.com>
2019-03-26 21:53:31 -0600

    Create heatmap instance with dummy data, nothing showing

---
+++ src/assets/jss/material-dashboard-react/views/mapsStyle.jsx
@@ -0,0 +1,20 @@
+import {
+  successColor,
+  whiteColor,
+  grayColor,
+  hexToRgb
+} from "assets/jss/material-dashboard-react.jsx";
+
+const mapsStyle = {
+  heatmapWrapper: {
+    width: "100% !important",
+    height: "100vh !important"
+  },
+  heatmapContainer: {
+    width: "100% !important",
+    height: "100vh !important",
+    zIndex: 50
+  }
+};
+
+export default mapsStyle;
--- src/views/Maps/Maps.jsx
+++ src/views/Maps/Maps.jsx
@@ -1,108 +1,74 @@
 import React from "react";
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
-import { h337 } from "./heatmap.min";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
 
+import GridItem from "components/Grid/GridItem.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
 
-const CustomSkinMap = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={13}
-      defaultCenter={{ lat: 40.748817, lng: -73.985428 }}
-      defaultOptions={{
-        scrollwheel: false,
-        zoomControl: true,
-        styles: [
-          {
-            featureType: "water",
-            stylers: [
-              { saturation: 43 },
-              { lightness: -11 },
-              { hue: "#0088ff" }
-            ]
-          },
-          {
-            featureType: "road",
-            elementType: "geometry.fill",
-            stylers: [
-              { hue: "#ff0000" },
-              { saturation: -100 },
-              { lightness: 99 }
-            ]
-          },
-          {
-            featureType: "road",
-            elementType: "geometry.stroke",
-            stylers: [{ color: "#808080" }, { lightness: 54 }]
-          },
-          {
-            featureType: "landscape.man_made",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#ece2d9" }]
-          },
-          {
-            featureType: "poi.park",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#ccdca1" }]
-          },
-          {
-            featureType: "road",
-            elementType: "labels.text.fill",
-            stylers: [{ color: "#767676" }]
-          },
-          {
-            featureType: "road",
-            elementType: "labels.text.stroke",
-            stylers: [{ color: "#ffffff" }]
-          },
-          { featureType: "poi", stylers: [{ visibility: "off" }] },
-          {
-            featureType: "landscape.natural",
-            elementType: "geometry.fill",
-            stylers: [{ visibility: "on" }, { color: "#b8cb93" }]
-          },
-          { featureType: "poi.park", stylers: [{ visibility: "on" }] },
-          {
-            featureType: "poi.sports_complex",
-            stylers: [{ visibility: "on" }]
-          },
-          { featureType: "poi.medical", stylers: [{ visibility: "on" }] },
-          {
-            featureType: "poi.business",
-            stylers: [{ visibility: "simplified" }]
-          }
-        ]
-      }}
-    >
-      <Marker position={{ lat: 40.748817, lng: -73.985428 }} />
-    </GoogleMap>
-  ))
-);
+import * as h337 from "heatmap.js";
 
-function Maps({ ...props }) {
-  // var heatmap = h337.create({
-  //   container: document.getElementById('heatmap')
-  // });
+import mapsStyle from "assets/jss/material-dashboard-react/views/mapsStyle.jsx";
 
-  // heatmap.setData({
-  //   max: 5,
-  //   data: [{ x: 10, y: 15, value: 5 }, { x: 5, y: 10, value: 10 }]
-  // });
+class Maps extends React.Component {
 
-  return (<div id="heatmap">
-    {/* <CustomSkinMap
-      googleMapURL="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: `100vh` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-    /> */}
-  </div>
+  constructor(props) {
+    super(props);
+    this.heatmapRef = React.createRef();
+  }
+
+  componentDidMount(){
+    const config = {
+      container: this.heatmapRef.current,
+    };
+    const heatmap = h337.create(config);
+    this.generate(heatmap);
+  }
+
+  generate(heatmap) {
+    var width = this.heatmapRef.current.offsetWidth;
+    var height = this.heatmapRef.current.offsetHeight;
     
-  );
+    var numSamples = 500;
+    // randomly generate extremas
+    var extremas = [Math.random() * numSamples, Math.random() * numSamples];
+    var max = Math.max.apply(Math, extremas);
+    var min = Math.min.apply(Math, extremas);
+    var data = [];
+
+    for (var i = 0; i < numSamples; i++) {
+      data.push({ 
+        x: Math.random() * width, 
+        y: Math.random() * height, 
+        value: (Math.random() * max - min) + min, 
+        // btw, we can set a radius on a point basis
+        radius: Math.random() * 80 
+      });
+    }
+    // set the generated dataset
+    heatmap.setData({
+      min: min,
+      max: max,
+      data: data
+    });
+  }
+
+  render(){
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.heatmapWrapper}>
+        <div 
+          className={classes.heatmapContainer} 
+          ref={this.heatmapRef}>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
-export default Maps;
+Maps.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(mapsStyle)(Maps);
