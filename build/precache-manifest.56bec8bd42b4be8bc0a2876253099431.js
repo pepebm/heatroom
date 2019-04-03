5a96853994099868c6c0f0118108563113ec5451
Jose Manuel Beauregard Mendez <jm.beauregard26@gmail.com>
2019-04-02 22:04:57 -0600

    resize heatmap

---
+++ build/precache-manifest.56bec8bd42b4be8bc0a2876253099431.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "b97e77a61e920bb667b0",
+    "url": "/static/css/main.55d11386.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "e98db541abfcbd398fa4",
+    "url": "/static/js/2.e98db541.chunk.js"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "b97e77a61e920bb667b0",
+    "url": "/static/js/main.b97e77a6.chunk.js"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "def1351188d20588385b190e82a651bf",
+    "url": "/static/media/reactlogo.def13511.png"
+  },
+  {
+    "revision": "e98db541abfcbd398fa4",
+    "url": "/static/css/2.55754dbc.chunk.css"
+  },
+  {
+    "revision": "ec807a706b6e66244e94aa6ac799ca71",
+    "url": "/index.html"
+  }
+];
